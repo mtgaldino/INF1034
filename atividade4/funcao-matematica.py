1800d90fd972f60a7502198bd80cc917e9522b8c
Matheus <matheusmanan@gmail.com>
2026-09-14 21:11:41 -0300

    concluído-atividade4

--- atividade4/funcao-matematica.py
+++ atividade4/funcao-matematica.py
@@ -37,6 +37,24 @@ def soma_10(x):
 def eleva_ao_quadrado(x):
     return x ** 2
 
+def raiz_quadrada(x):
+    return x ** 0.5
+
+def x_sobre_1(x):
+    return 1 / x
+
+def potencia_de_2(x):
+    return 2 ** x
+
+def cinco_menos_x_quadrado(x):
+    return 5 - x ** 2
+
+def fn_quadratica(x):
+    return x ** 2 - 5 * x + 6
+
+def fn_cubica(x):
+    return x ** 3 - x ** 2 - x + 1
+
 # === programa ===
 desenhar_plano_cartesiano()
 
@@ -52,22 +70,98 @@ limpar_tela()
 
 t.color("red")
 t.pu()
-t.goto(-20, eleva_ao_quadrado(-20))
+t.goto(10 * -20, eleva_ao_quadrado(-20))
 t.pd()
 
-for x in range(-99, 101):
-    t.goto(x, eleva_ao_quadrado(x))
+for x in range(-20, 21):
+    t.goto(10 * x, eleva_ao_quadrado(x))
 
+limpar_tela()
 
+# A.
 
+# - y = √x
+t.color("green")
+t.pu()
+t.goto(0, 40 * raiz_quadrada(0))
+t.pd()
 
+for x in range(0, 41):
+    t.goto(10 * x, 40 * raiz_quadrada(x))
 
+limpar_tela()
 
+# B.
 
+# - y = 1/x
+t.color("purple")
 
+# x sendo negativo
+t.pu()
+t.goto(10 * -40, 200 * x_sobre_1(-40))
+t.pd()
+for x in range(-40, 0):
+    t.goto(10 * x, 200 * x_sobre_1(x))
 
+# x sendo positivo
+t.pu()
+t.goto(10 * 1, 200 * x_sobre_1(1))
+t.pd()
+for x in range(1, 41):
+    t.goto(10 * x, 200 * x_sobre_1(x))
 
+limpar_tela()
 
+# C.
 
+# - y = 2^x
+t.color("orange")
+t.pu()
+t.goto(40 * -10, potencia_de_2(-10))
+t.pd()
+
+for x in range(-10, 9):
+    t.goto(40 * x, potencia_de_2(x))
+
+limpar_tela()
+
+# D.
+ 
+# - y = 5 - x^2
+t.color("brown")
+t.pu()
+t.goto(10 * -20, cinco_menos_x_quadrado(-20))
+t.pd()
+
+for x in range(-20, 21):
+    t.goto(10 * x, cinco_menos_x_quadrado(x))
+
+limpar_tela()
+
+# E.
+
+# - y = x^2 - 5x + 6
+t.color("navy")
+t.pu()
+t.goto(10 * -15, fn_quadratica(-15))
+t.pd()
+
+for x in range(-15, 21):
+    t.goto(10 * x, fn_quadratica(x))
+
+limpar_tela()
+
+# F.
+
+# - y = x^3 - x^2 - x + 1
+t.color("magenta")
+t.pu()
+t.goto(40 * -7, fn_cubica(-7))
+t.pd()
+
+for x in range(-7, 8):
+    t.goto(40 * x, fn_cubica(x))
+
+limpar_tela()
 
 mainloop()
